bc63d59c70754ad9e569d8d685af9dddcc20a6dd
hudaanees <139462660+hudaanees@users.noreply.github.com>
2023-10-29 06:55:05 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+//C program to print all unique elements in an array.
+int main()
+{
+int arr[100], unique[100];
+    int n, uniqueCount = 0;
 
-//c program to convert upper cases into lower and vice versa.
-int main() {
-    char sentence[1000];
-    int i;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
 
-    printf("Enter a sentence: ");
-    gets(sentence);
+    if (n <= 0) {
+        printf("Array size should be greater than 0.\n");
+        return 1;
+    }
 
-for (i = 0; sentence[i] != '\0'; i++) {
-        if (islower(sentence[i])) {
-            sentence[i] = toupper(sentence[i]);
-        } else if (isupper(sentence[i])) {
-            sentence[i] = tolower(sentence[i]);
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+for (int i = 0; i < n; i++) {
+        int isUnique = 1;
+        for (int j = 0; j < uniqueCount; j++) {
+            if (arr[i] == unique[j]) {
+                isUnique = 0;
+                break;
+            }
+        }
+        if (isUnique) {
+            unique[uniqueCount] = arr[i];
+            uniqueCount++;
         }
     }
 
-    printf("change sentence: %s\n", sentence);
-
+    printf("Unique elements in the array: ");
+    for (int i = 0; i < uniqueCount; i++) {
+        printf("%d ", unique[i]);
+    }
+    printf("\n");
     return 0;
 }
